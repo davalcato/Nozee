696dad94d85a74cafa27906dfca1bb6e3c84f408
davalcato <davalcato@yahoo.com>
2019-07-03 21:56:52 -0700

    I specified the frame view in the collectionView

--- Nozee/ViewController.swift
+++ Nozee/ViewController.swift
@@ -18,7 +18,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(collectionView)
+        collectionView.frame = view.frame
+       
     }
 
 
