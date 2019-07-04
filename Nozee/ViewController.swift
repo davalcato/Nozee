3277be63d6fd8c0ba6a8aea864b2659f342c086b
davalcato <davalcato@yahoo.com>
2019-07-03 21:34:07 -0700

    first commit

---
+++ Nozee/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Nozee
+//
+//  Created by Daval Cato on 7/3/19.
+//  Copyright © 2019 Daval Cato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let collectionView: UICollectionView = {
+        
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
