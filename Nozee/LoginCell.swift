c0c7d3af095d35598728f5fe093e01db30e81c87
davalcato <davalcato@yahoo.com>
2019-07-08 18:19:23 -0700

    added the LeftPaddedTextField and right to the LoginCell

--- Nozee/LoginCell.swift
+++ Nozee/LoginCell.swift
@@ -53,18 +53,31 @@ class LoginCell: UICollectionViewCell {
         addSubview(logoImageView)
         addSubview(emailTextField)
         addSubview(passwordTextField)
+        addSubview(loginButton)
         
-        _ = logoImageView.anchor(centerYAnchor, left: nil, bottom: nil, right: nil, topConstant: -200, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 160, heightConstant: 160)
+        _ = logoImageView.anchor(centerYAnchor, left: nil, bottom: nil, right: nil, topConstant: -220, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 160, heightConstant: 160)
         logoImageView.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
         
         _ = emailTextField.anchor(logoImageView.bottomAnchor, left: leftAnchor, bottom: nil, right: rightAnchor, topConstant: 8, leftConstant: 32, bottomConstant: 0, rightConstant: 32, widthConstant: 0, heightConstant: 50)
         
         _ = passwordTextField.anchor(emailTextField.bottomAnchor, left: leftAnchor, bottom: nil, right: rightAnchor, topConstant: 16, leftConstant: 32, bottomConstant: 0, rightConstant: 32, widthConstant: 0, heightConstant: 50)
         
+        _ = loginButton.anchor(passwordTextField.bottomAnchor, left: leftAnchor, bottom: nil, right: rightAnchor, topConstant: 16, leftConstant: 32, bottomConstant: 0, rightConstant: 32, widthConstant: 0, heightConstant: 50)
+        
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+}
+
+class LeftPaddedTextField: UITextField {
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        <#code#>
+    }
+    
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        <#code#>
     }
 }
 
