aff57db0b833014b58c8b5d53c1a44db71907a57
davalcato <davalcato@yahoo.com>
2019-07-08 17:08:02 -0700

    I registered the LoginCell in func registerCell in the ViewController file
            collectionView.register(PageCell.self, forCellWithReuseIdentifier: cellId)
            collectionView.register(LoginCell.self, f

---
+++ Nozee/LoginCell.swift
@@ -0,0 +1,39 @@
+//
+//  LoginCell.swift
+//  Nozee
+//
+//  Created by Daval Cato on 7/8/19.
+//  Copyright © 2019 Daval Cato. All rights reserved.
+//
+
+import UIKit
+
+class LoginCell: UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = .green
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
